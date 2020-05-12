c4ff1992b8c75efdd636bbffe6d590c8f3da5944
soramin <44796681+soramin@users.noreply.github.com>
2020-05-12 19:30:59 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace List8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int result = 0;
+
+            Console.WriteLine("整数を入力してください");
+
+            string strNo = Console.ReadLine();
+
+            x = int.Parse(strNo);
+
+            if(!int.TryParse(strNo, out result))
+            {
+                Console.WriteLine("整数が入力されなかったため処理を終了します。");
+                return;
+            }
+
+            Console.WriteLine($"入力した値は{result}です。");
+        }
+    }
+}
